4f75f939940359bf0a4708cf8362ae01e743e277
Choi-Minju-2024web <0412chmj@gmail.com>
2025-10-04 17:25:13 +0900

    Update DNNtest.py

--- 코드 정리/카메라 인식 부분/DNNtest.py
+++ 코드 정리/카메라 인식 부분/DNNtest.py
@@ -227,7 +227,7 @@ q = queue.Queue()
 serial_thread = threading.Thread(target=serial_worker, args=(q,), daemon=True)
 serial_thread.start()
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 if not cap.isOpened():
